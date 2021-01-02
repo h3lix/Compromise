01cba8bde8aecce025ac90b581f4cc691ea91390
manjaro <harry.miller@warwick.ac.uk>
2021-01-02 17:58:31 +0000

    Added the basis for a training class & fixed CompromiseGame so that newPlayers actually changed the players

--- scr/NNPlayer.py
+++ scr/NNPlayer.py
@@ -13,19 +13,67 @@ class NNPlayer(cg.AbstractPlayer):
         nn_inputs.extend((my_score, opp_score, turn, length, num_pips))
         return np.floor(self.brain.forward(nn_inputs) * 3).tolist()[0]
 
+    def calculate_fitness(self, my_score, opp_score):
+        self.fitness = (my_score * 2) - opp_score
+        if my_score > opp_score:
+            self.fitness += 5
+
+class GeneticAlgorithm:
+    def __init__(self, population_size):
+        self.population = [NNPlayer() for _ in range(population_size)]
+
+    def play_random(self, num_of_games):
+        games = [cg.CompromiseGame(pA, pB, 30, 10) for _ in range(num_of_games)]
+
+        while len(self.population) > 1:
+            random.shuffle(self.population)
+
+            player_A = self.population.pop()
+            player_B = self.population.pop()
+
+
 if __name__ == "__main__":
-    population_size = 10
+    pA = NNPlayer()
+    pB = NNPlayer()
+
+    g = cg.CompromiseGame(pA, pB, 30, 10)
+
+    print(g.play(), g.greenPlayer, g.redPlayer)
+
+    g.newPlayers(cg.GreedyPlayer(), cg.RandomPlayer())
+
+    print(g.play(), g.greenPlayer, g.redPlayer)
+
+    '''
+    population_size = 20
     population = [NNPlayer() for _ in range(population_size)]
 
+    played = []
+
     while len(population) > 1:
-        played = []
         random.shuffle(population)
 
 
         pA = population.pop()
         pB = population.pop()
         g = cg.CompromiseGame(pA, pB, 30, 10)
-        g.play()
+        scores = g.play()
+
+        pA.calculate_fitness(scores[0], scores[1])
+        pB.calculate_fitness(scores[1], scores[0])
 
         played.extend((pA, pB))
         print(g.greenScore, g.redScore)
+
+    fitnesses = np.array([p.fitness for p in played])
+    norm_fitnesses = fitnesses / sum(fitnesses)
+    print(fitnesses, norm_fitnesses)
+    parents = np.random.choice(played, len(played)//2, replace=False, p=norm_fitnesses)
+
+    for parent in parents:
+        print(parent.fitness)
+
+    #sorted_population = sorted(played, key=lambda player: -player.fitness)
+    #for player in sorted_population:
+    #    print(player.fitness)
+    '''
